4caabe911dc82c3e8f2eca74575f90c8a9c7cd16
Zied Maaloul <zimaaloul@gmail.com>
2021-12-18 08:43:30 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BinToIeee msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import BinToIeee from './components/BinToIeee'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    BinToIeee
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
